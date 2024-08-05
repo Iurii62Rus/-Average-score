9f5ac63269160961da187ed398a8de4a64d8e78f
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-08-05 13:39:51 +0300

    Add files via upload

---
+++ Average score.py
@@ -0,0 +1,12 @@
+< len(grades):
+averages[student] = calculate_average(grades[i])
+else:
+averages[student] = 0
+print(averages)
+grades = [[1, 2, 3, 4, 0, 5], [4, 4, 3, 5], [3, 2, 5, 4, 1], [5, 5, 3, 4, 3], [5, 5, 4, 3, 3]]
+students = ['Pablo Escobar', 'Grigory Rasputin', 'Yuri Kharlamov', 'Peter the First', 'Iiiigar']
+def calculate_average(grades):
+    return sum(grades) / len(grades)
+averages = {}
+for i, student in enumerate(students):
+    if i
